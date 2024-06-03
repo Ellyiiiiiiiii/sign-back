d82d398f9f126f2d997cbf9d5dbe41ae13b61f4a
Steven <steven@ispan.com.tw>
2024-06-03 15:21:03 +0800

    CJS 匯入匯出

--- src/person.cjs
+++ src/person.cjs
@@ -8,8 +8,12 @@ class Person {
   }
 }
 
+
 const p1 = new Person("John", 25);
 console.log(p1);
 console.log(p1 + '');
 
 
+// CJS 的匯出, 可以匯出任何類型的資料
+// 但只能有一個
+module.exports = Person;
---
+++ src/try-person.cjs
@@ -0,0 +1,8 @@
+
+
+// CJS 匯入
+const Person = require('./Person.cjs');
+
+const p2 = new Person("Flora", 23);
+
+console.log(p2);
