e8f7b9f0dd661f4db6c0b0bfa92109ac9e28e5d0
Steven <steven@ispan.com.tw>
2024-06-04 14:26:20 +0800

    ejs 安裝測試

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 // 路由設定, routes
 // 1. get(): 只接受 HTTP GET 方法的拜訪
 // 2. 只接受 路徑為 / 的 request
 app.get("/", (req, res) => {
-  res.send("<h2>Hello World</h2>");
+  // res.send("<h2>Hello World</h2>");
+  res.render("home", { name: "Shinder" });
 });
 
 // ************* 設定靜態內容資料夾 *************
