7613cf95d9c7909e16c62e964a560433230ce7fe
Steven <steven@ispan.com.tw>
2024-06-11 15:22:28 +0800

    取得總筆數

--- routes/address-book.js
+++ routes/address-book.js
@@ -1,10 +1,16 @@
 import express from "express";
+import db from "./../utils/connect-mysql.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   res.locals.pageName = "ab-list";
-  res.render("address-book/list");
+
+  const sql = "SELECT COUNT(*) totalRows FROM address_book";
+  const [[{ totalRows }]] = await db.query(sql); // 取得總筆數
+  res.json(totalRows);
+
+  // res.render("address-book/list");
 });
 
 export default router;
