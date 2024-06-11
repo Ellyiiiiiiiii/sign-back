f44686e68848b73fcddbdabf5c0bee30ed98ce0b
Steven <steven@ispan.com.tw>
2024-06-11 16:11:29 +0800

    分頁資料 1

--- routes/address-book.js
+++ routes/address-book.js
@@ -6,9 +6,24 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   res.locals.pageName = "ab-list";
 
+  const perPage = 20; // 每頁最多有幾筆
+  let page = +req.query.page || 1;
+
   const sql = "SELECT COUNT(*) totalRows FROM address_book";
   const [[{ totalRows }]] = await db.query(sql); // 取得總筆數
-  res.json(totalRows);
+
+  let totalPages = 0; // 總頁數, 預設值設定 0
+  let rows = []; // 分頁資料
+  if (totalRows > 0) {
+    totalPages = Math.ceil(totalRows / perPage);
+
+    const sql2 = `SELECT * FROM address_book LIMIT ${
+      (page - 1) * perPage
+    }, ${perPage} `;
+    [rows] = await db.query(sql2);
+  }
+
+  res.json({ totalRows, totalPages, page, perPage, rows });
 
   // res.render("address-book/list");
 });
