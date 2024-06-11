1a06852a35e7de69423c22734ea783cb0bf919e2
Steven <steven@ispan.com.tw>
2024-06-11 15:11:13 +0800

    address-book list 頁

--- routes/address-book.js
+++ routes/address-book.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("<h1>ab test</h1>");
+  res.render("address-book/list");
 });
 
 export default router;
